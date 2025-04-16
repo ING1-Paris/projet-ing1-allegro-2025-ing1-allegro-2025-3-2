4cfc3f4a51a96fad97cd347dda2e69a6ba18292f
hortense2006 <hortense.galtier@edu.ece.fr>
2025-04-16 13:01:19 +0200

    update

--- signin.c
+++ signin.c
@@ -1,12 +1,12 @@
 // Created by galti on 15/04/2025.
 #include "signin.h"
-
+#include <stdio.h>
 int signin(t_joueur *joueur)
 {
     // PSEUDO
     printf("Choisissez un pseudo:\n");
-    scanf("%c", &joueur->user);
-    if (joueur->user < 1 || joueur->user > 20){
+    scanf("%c", joueur->user);
+    if (*joueur->user < 1 || joueur->user > 20){
       printf("Votre pseudo n'a pas le nombre de caracteres requis.\n");
     }
     else{         
