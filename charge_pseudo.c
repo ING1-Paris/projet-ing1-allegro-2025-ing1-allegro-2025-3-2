3f90745b2e24cadeed852f6051410dd427bb545b
hortense2006 <hortense.galtier@edu.ece.fr>
2025-04-17 12:02:03 +0200

    update "charge_pseudo.c"

--- charge_pseudo.c
+++ charge_pseudo.c
@@ -4,58 +4,66 @@
 #include <stdlib.h>
 #include <string.h>
 #include "signin.h"
-char charge_pseudo(t_joueur *joueur){
-  /*ON VERIFIE SI LE PSEUDO EXISTES*/
+char charge_pseudo(t_joueur *joueur)
+{
+
+
+
+
+  /* ENREGISTREMENT DU PSEUDO*/
+
+  FILE* AjoutPseudo = fopen("./donnees_sauvegardees/pseudos.txt", "a"); // Ouverture du fichier pseudos.txt en mode ajout
+  if (AjoutPseudo == NULL) { // Erreur d'ouverture de fichier
+    printf("Erreur d'ouverture de fichier\n");
+    exit(1);
+  }
+  fprintf(AjoutPseudo, "%s", joueur->user);
+  printf("Votre pseudo \"%s\" a ete sauvegarde.\n", joueur->user);
+  fclose(AjoutPseudo);
+  AjoutPseudo = NULL;
+
+  return 0;
+}
+char verifie_pseudo(t_joueur *joueur) {
   /*DEFINITION DES VARIABLES*/
   char pseudos[NOMBRE_MAX_PSEUDOS][LONGUEUR_MAX_PSEUDO]; // Tableau de pseudos
   int numero = 0;
-  int test = 0;
+  int comparer = 0;
+  int i=0;
 
-  // On lit les pseudos
-  FILE* Pseudos = fopen("./donnees_sauvegardees/pseudos.txt", "r"); // Ouverture du fichier pseudos.txt en mode lecture
+  /* OUVERTURE DU FICHIER EN MODE LECTURE*/
+  FILE* Pseudos = fopen("./donnees_sauvegardees/pseudos.txt", "r");
   if (Pseudos == NULL) { // Erreur d'ouverture de fichier
     printf("Erreur d'ouverture de fichier");
     exit(1);
   }
+  /* COMPTABILISE LE NOMBRE DE PSEUDOS ENREGISTRES*/
   while (fscanf(Pseudos, "%s", pseudos[numero]) == 1){
     numero++;
   }
-  fclose(Pseudos);
-  Pseudos = NULL;
+  printf("%d",numero); // ligne de vérification dans la console
 
-  // On vérifie si le pseudo entré existe déjà
-  for (int i = 0; i <= numero; i++) { // On parcourt toutes les lignes
-    test = strcmp(pseudos[i], &joueur->user); // Comparaison entre pseudo_lus[i] et pseudo
-    if (test == 0) { // Dans le cas où pseudo_lus[i] = pseudo
-      printf("Erreur : Le pseudo que vous avez saisi : \"%s\" existe deja.\n", &joueur->user);
-      exit(1);
+  /* VERIFICATION DE L'EXISTENCE DU PSEUDO*/
+  for (i = 0; i <= numero; i++)
+  { // On parcourt toutes les lignes
+    comparer = strcmp(pseudos[i], joueur->user); // Comparaison entre pseudos[i] et joueur-> user
+    if (comparer == 0) { // pseudos[i] = joueur->user
+      i++; // On va à la ligne suivante
+    }
+    else // le pseudo ne fait pas partie de la liste des pseudos enregistrés
+    {
+      //On sort de la boucle for
+      return 0;
     }
   }
-  // Situation où le pseudo n'existe pas déjà :
-  // 1) on ajoute le pseudo dans pseudos.txt
-  // (dans pFichierPseudosA, le A est pour "Add")
-  FILE* pFichierPseudosA = fopen("./donnees_sauvegardees/pseudos.txt", "a"); // Ouverture du fichier pseudos.txt en mode ajout
-  if (pFichierPseudosA == NULL) { // Erreur d'ouverture de fichier
-    printf("Erreur d'ouverture de fichier\n");
-    exit(1);
+  if (i==numero) {
+    printf("Le pseudo n'existes pas encore.");
+    return 1;
   }
-  fprintf(pFichierPseudosA, "%s", &joueur->user);
-  fprintf(pFichierPseudosA, "%s", "\n");
-  printf("Votre pseudo \"%s\" a ete sauvegarde.\n", &joueur->user);
-  fclose(pFichierPseudosA);
-  pFichierPseudosA = NULL;
-  // 2) Enregistrement de 0 à la ligne correspondant au joueur dans scores.txt
-  FILE* pFichierScoreA = fopen("./donnees_sauvegardees/scores.txt", "a");
-  if (pFichierScoreA == NULL) { // Erreur d'ouverture de fichier
-    printf("Erreur d'ouverture de fichier");
-    exit(1);
+  else {
+    //On ferme le fichier
+    fclose(Pseudos);
+    Pseudos = NULL;
+    return 0;
   }
-  fprintf(pFichierScoreA, "%d", 0);
-  fprintf(pFichierScoreA, "%s", "\n");
-  fclose(pFichierScoreA);
-  pFichierScoreA = NULL;
-  //Si oui, on ne fait rien
-  // Si non, on l'ajoutes à la liste des pseudos
-
-  return 0;
 }
