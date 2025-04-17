07fc670eacf092a56c0121a5b4959b8c288dcf2c
hortense2006 <hortense.galtier@edu.ece.fr>
2025-04-17 12:11:46 +0200

    update "charge_mdp.c"

--- charge_mdp.c
+++ charge_mdp.c
@@ -1,3 +1,23 @@
-/
 // Created by galti on 16/04/2025.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "signin.h"
 
+char charge_mdp(t_joueur *joueur)
+{
+
+    /* ENREGISTREMENT DU PSEUDO*/
+
+    FILE* AjoutPseudo = fopen("./donnees_sauvegardees/pseudos.txt", "a"); // Ouverture du fichier pseudos.txt en mode ajout
+    if (AjoutPseudo == NULL) { // Erreur d'ouverture de fichier
+        printf("Erreur d'ouverture de fichier\n");
+        exit(1);
+    }
+    fprintf(AjoutPseudo, "%s", joueur->user);
+    printf("Votre pseudo \"%s\" a ete sauvegarde.\n", joueur->user);
+    fclose(AjoutPseudo);
+    AjoutPseudo = NULL;
+
+    return 0;
+}
