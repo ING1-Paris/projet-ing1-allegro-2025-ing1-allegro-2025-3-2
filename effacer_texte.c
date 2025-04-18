e6d971b2a75af0b45909afb915315e991692495b
hortense2006 <hortense.galtier@edu.ece.fr>
2025-04-18 14:01:41 +0200

    update "effacer_texte.c"

--- effacer_texte.c
+++ effacer_texte.c
@@ -5,13 +5,15 @@
 // Effacer la zone du texte en restaurant la partie du fond correspondant
 char effacer_texte(BITMAP*background,char *texte){
     int couleur_effacee = makecol(0,0,0);// couleur noire
+    int x = SCREEN_W/2; // abscisses de début du texte
+    int y = SCREEN_H/2; // ordonnées du début du texte
     /* HAUTEUR DU TEXTE*/
     int hauteur_zone = text_height(font)+10;
     /* LARGEUR DU TEXTE*/
     int largeur_zone = text_length(font,texte);
     /* RECTANGLE NOIR COUVRANT LE TEXTE*/
-    rectfill(background, x, y, x+largeur_zone, y+hauteur_zone, couleur_effacee);
+    rectfill(screen, x-largeur_zone/2, y-hauteur_zone/2, x+largeur_zone/2, y+hauteur_zone/2+20, couleur_effacee);
     /* RESTAURATION DU FOND D'ECRAN*/
-    blit(background, screen, x, y, x, y, largeur_zone, hauteur_zone);
+    blit(screen, background, x, y, x, y, largeur_zone, hauteur_zone);
     return 0;
 }
