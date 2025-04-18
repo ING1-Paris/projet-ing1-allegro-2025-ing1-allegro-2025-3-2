3b7c941687ec9730fbb267d33c8acb3fe31f3fa2
hortense2006 <hortense.galtier@edu.ece.fr>
2025-04-18 11:08:41 +0200

    update "ecrire_texte.c"

--- ecrire_texte.c
+++ ecrire_texte.c
@@ -1,20 +1,8 @@
-//
 // Created by galti on 17/04/2025.
 #include <stdio.h>
 #include <allegro.h>
 #include "signin.h"
-// Effacer la zone du texte en restaurant la partie du fond correspondant
-void effacer_texte(t_joueur *joueur,BITMAP*background){
-    int couleur_effacee = makecol(0,0,0);// couleur noire
-    //Cela fait s'effacer le texte
-    // On détermine la largeur du texte
-    int largeur_zone = text_length(font, "") + 10;
-    // On calcule la hauteur du texte
-    int hauteur_zone = text_height(font)+10;
-    //On restaure le fond d'écran de la zone effacée
-    blit(background, screen, x, y, x, y, largeur_zone, hauteur_zone);
 
-}
 
 char ecrire_texte(t_joueur *joueur,char name) {
     /*DEFINITION DES VARIABLES*/
