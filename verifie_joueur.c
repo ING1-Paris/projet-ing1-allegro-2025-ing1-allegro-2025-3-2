1f3cac42133b69cf24cf7371885451b091e9c2a7
hortense2006 <hortense.galtier@edu.ece.fr>
2025-04-18 12:37:25 +0200

    update "verifie_joueur.c"

--- verifie_joueur.c
+++ verifie_joueur.c
@@ -30,5 +30,64 @@ char verifie_joueur(t_joueur *joueur, char name)
     }
     fclose(Joueurs);
     Joueurs = NULL;
+    if (name[5] == "user")
+    {
+        for (int i=0;i<20;i++)
+        { // On fixe une limite de caractères pour le pseudo
+            while (!key[KEY_ENTER])
+            {// On ne s'arrête que quand le joueur appuie sur la touche ENTER
+                //Retourne la touche appuyée
+                readkey();
+                if ((readkey()>>8) >= KEY_A && (readkey()>>8) <= KEY_Z)
+                { // si le scancode de la touche appuyée est entre A et Z
+                    //On l'ajoute à joueur->user[i]
+                    key[readkey()>>8] = joueur->user[i];
+                    //On l'affiche avec textout
+                    textout_centre_ex(screen,font,joueur->user,x,y+20,couleur_texte,-1);
+                    //On met à jour i
+                    i++;
+                }
+                else
+                {
+                    //Quoi qu'il arrive, on met à jour i
+                    i++;
+                }
+                return 0;
+            }
+            return 0;
+        }
+        return joueur->user;
+    }
+
+    /* SAISIE DU MOT DE PASSE*/
+
+    if (name[5] == "mdp")
+    {
+        for (int i=0;i<12;i++)
+        { // On fixe une limite de caractères pour le mot de passe
+            while (!key[KEY_ENTER])
+            {// On ne s'arrête que quand le joueur appuie sur la touche ENTER
+                //Retourne la touche appuyée
+                readkey();
+                if ((readkey()>>8) >= KEY_A && (readkey()>>8) <= KEY_Z)
+                { // si le scancode de la touche appuyée est entre A et Z
+                    //On l'ajoute à joueur->user[i]
+                    key[readkey()>>8] = joueur->mdp[i];
+                    //On l'affiche avec textout
+                    textout_centre_ex(screen,font,joueur->mdp,x,y+20,couleur_texte,-1);
+                    //On met à jour i
+                    i++;
+                }
+                else
+                {
+                    //Quoi qu'il arrive, on met à jour i
+                    i++;
+                }
+                return 0;
+            }
+            return 0;
+        }
+        return joueur->mdp;
+    }
     return 0;
 }
