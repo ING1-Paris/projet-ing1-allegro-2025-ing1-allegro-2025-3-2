89d1e0f8c7feda8ccdb7e60863ee027c02b9a7ad
hortense2006 <hortense.galtier@edu.ece.fr>
2025-04-18 12:33:36 +0200

    update "charge_pseudo.c"

--- charge_pseudo.c
+++ charge_pseudo.c
@@ -5,38 +5,6 @@
 #include <string.h>
 #include "signin.h"
 
-
-char verifie_pseudo(t_joueur *joueur)
-{
-  /*DEFINITION DES VARIABLES*/
-  char pseudos[NOMBRE_MAX_PSEUDOS][LONGUEUR_MAX_PSEUDO]; // Tableau de pseudos
-  int i=0;
-
-  /* OUVERTURE DU FICHIER EN MODE LECTURE*/
-  FILE* Joueurs = fopen("./donnees_sauvegardees/joueur.txt", "r");
-  if (Joueurs == NULL)
-  { // Erreur d'ouverture de fichier
-    printf("Erreur d'ouverture de fichier");
-    exit(1);
-  }
-  while (fscanf(Joueurs, "%s %s", joueur->user,joueur->mdp) != EOF) {
-    // Tant que la liste n'est pas finie, on compare le pseudo & le mdp donné avec la liste
-    if (joueur->user == pseudos[i] && joueur->mdp == pseudos[i])
-    {
-      //Le pseudo & son mot de passe associé font partie de la liste des pseudos enregistrés
-      return 0;
-    }
-    else
-    {
-      i++;
-    }
-  }
-  fclose(Joueurs);
-  Joueurs = NULL;
-  return 0;
-}
-
-
 char charge_joueur(t_joueur *joueur)
 {
   FILE* AjoutJoueur = fopen("./donnees_sauvegardees/joueur.txt", "a"); // Ouverture du fichier pseudos.txt en mode ajout
