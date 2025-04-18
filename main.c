e2bb3c88ed789748e19137128a7284658ac24ccd
hortense2006 <hortense.galtier@edu.ece.fr>
2025-04-18 14:22:39 +0200

    add "main.c"

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <allegro.h>
+#include "signin.h"
+void initialisation_allegro();
+int main()
+{
+    BITMAP*background;
+    t_joueur *joueur;
+    char name[5];
+    initialisation_allegro();
+    background = load_bitmap("./images/background.bmp",NULL);
+    if (key[KEY_ESC]) {
+        login(joueur,background,name);
+    }
+    else {
+        signin(joueur,background);
+    }
+}END_OF_MAIN();
+
+void initialisation_allegro() {
+    allegro_init(); // appel obligatoire (var.globales, recup. infos syst me ...)
+    install_keyboard(); //pour utiliser le clavier
+    install_mouse(); //pour utiliser la souris
+    //pour choisir la profondeur de couleurs (8,16,24 ou 32 bits)
+    set_color_depth(desktop_color_depth()); //ici : identique à celle du bureau
+
+    //sélection du mode graphique
+    // avec choix d'un driver+mode+résolution de l'écran
+    /// si échec, le programme s'arrête
+    if(set_gfx_mode(GFX_AUTODETECT_WINDOWED,800,600,0,0)!=0)
+    {
+        allegro_message("probleme mode graphique");
+        allegro_exit();
+        exit(EXIT_FAILURE);
+    }
+}
