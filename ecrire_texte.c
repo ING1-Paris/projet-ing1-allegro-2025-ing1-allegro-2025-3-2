4f9ac7a9f01ff671e329b91fc0fad751390f22e9
hortense2006 <hortense.galtier@edu.ece.fr>
2025-04-19 10:13:40 +0200

    update "ecrire_texte.c"

--- ecrire_texte.c
+++ ecrire_texte.c
@@ -18,12 +18,10 @@ char ecrire_texte(t_joueur *joueur,char *name) {
         { // On fixe une limite de caractères pour le pseudo
             while (!key[KEY_ENTER])
             {// On ne s'arrête que quand le joueur appuie sur la touche ENTER
-                //Retourne la touche appuyée
-                readkey();
+                readkey();//Retourne la touche appuyée
                 if ((readkey()>>8) >= KEY_A && (readkey()>>8) <= KEY_Z)
-                { // si le scancode de la touche appuyée est entre A et Z
-                    //On l'ajoute à joueur->user[i]
-                    key[readkey()>>8] = joueur->user[i];
+                { // si le scancode de la touche appuyée est entre A et Z : on l'ajoute à joueur->user[i]
+                    joueur->user[i] = key[readkey()>>8];
                     //On l'affiche avec textout
                     textout_centre_ex(screen,font,joueur->user,x,y+20,couleur_texte,-1);
                     //On met à jour i
@@ -31,30 +29,26 @@ char ecrire_texte(t_joueur *joueur,char *name) {
                 }
                 else
                 {
-                    //Quoi qu'il arrive, on met à jour i
+                    // On met à jour i
                     i++;
                 }
-                return 0;
             }
-            return 0;
         }
         return joueur->user;
     }
 
     /* SAISIE DU MOT DE PASSE*/
 
-    if (strcpy(name,"mdp") == 0)
+    if (strcmp(name,"mdp") == 0)
     {
         for (int i=0;i<12;i++)
         { // On fixe une limite de caractères pour le mot de passe
             while (!key[KEY_ENTER])
             {// On ne s'arrête que quand le joueur appuie sur la touche ENTER
-                //Retourne la touche appuyée
-                readkey();
+                readkey();//Retourne la touche appuyée
                 if ((readkey()>>8) >= KEY_A && (readkey()>>8) <= KEY_Z)
-                { // si le scancode de la touche appuyée est entre A et Z
-                    //On l'ajoute à joueur->user[i]
-                    key[readkey()>>8] = joueur->mdp[i];
+                { // si le scancode de la touche appuyée est entre A et Z : on l'ajoute à joueur->user[i]
+                    joueur->mdp[i] = key[readkey()>>8];
                     //On l'affiche avec textout
                     textout_centre_ex(screen,font,joueur->mdp,x,y+20,couleur_texte,-1);
                     //On met à jour i
@@ -65,11 +59,13 @@ char ecrire_texte(t_joueur *joueur,char *name) {
                     //Quoi qu'il arrive, on met à jour i
                     i++;
                 }
-                return 0;
             }
-            return 0;
         }
         return joueur->mdp;
+    }
+    if (strcmp(name,"user") != 0 && strcmp(name,"mdp") != 0)
+    {
+        return 1;
     }
     return 0;
 }
